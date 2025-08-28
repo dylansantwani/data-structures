69c10f3c8bed6a44e5b551e2015ca167e72ad7ca
Dylan <144082336+dylansantwani@users.noreply.github.com>
2025-08-28 13:14:08 -0500

    add reverse program

--- Chapter 15 Activities/Downsize/src/Business.java
+++ Chapter 15 Activities/Downsize/src/Business.java
@@ -13,6 +13,6 @@ public class Business
      */
     public static void downsize(LinkedList<String> employeeNames, int n)
     {
-        ...
+        
     }
 }
--- Chapter 15 Activities/Reverse/src/ListUtil.java
+++ Chapter 15 Activities/Reverse/src/ListUtil.java
@@ -2,16 +2,20 @@ import java.util.LinkedList;
 
 /**
  * This class supplies a utility method to reverse the entries in a linked list.
-*/
-public class ListUtil
-{
+ */
+public class ListUtil {
     /**
      * Reverses the elements in a linked list
      *
      * @param strings the linked list to reverse
-    */
-    public static void reverse(LinkedList<String> strings)
-    {
-        ...
+     */
+    public static void reverse(LinkedList<String> strings) {
+        int initalsize = strings.size();
+        for (int i = 0; i < initalsize; i++) {
+            strings.addLast(strings.get((initalsize- 1) - i));
+        }
+        for (int i = 0; i < initalsize; i++) {
+            strings.removeFirst();
+        }
     }
 }
