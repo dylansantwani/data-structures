25a8de48cdd9b5e496ea59e1ac88a0fb416e9f31
Dylan <144082336+dylansantwani@users.noreply.github.com>
2025-09-11 13:05:40 -0500

    finish string length map

--- Chapter 15 Activities/StringLengthMap/src/StringLengthMap2.java
+++ Chapter 15 Activities/StringLengthMap/src/StringLengthMap2.java
@@ -12,26 +12,31 @@ public class StringLengthMap2
 {
     public static void main(String[] args)
     {
-        String filename = "src/test1.txt";
+        String filename = "C:\\Users\\dasantwani\\Documents\\GitHub\\data-structures\\Chapter 15 Activities\\StringLengthMap\\src\\test1.txt";
 
         try (Scanner in = new Scanner(new File(filename)))
         {
 
-            // Create your map here
+            Map<Integer, String> map = new HashMap<>();
             
 
             while (in.hasNext())
             {
                 String word = clean(in.next());
                 Integer len = word.length();
-
+                map.merge(len, word, (a, b) -> a + "," + b);
+                System.out.println(map);
                 // Update the map here
                 // Use the Java 8 merge() method
                 
 
 
             }
+            for (Integer key : map.keySet()) {
+                String value = map.get(key); 
+                System.out.println(key + ":" + value);
 
+            }
             // Print the strings, in increasing order of their length
             // Use this format: 1: i, a, i
         } catch (FileNotFoundException e)
