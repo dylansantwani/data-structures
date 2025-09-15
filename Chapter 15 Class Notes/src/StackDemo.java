39fc8413453cd53c8009fa4ceb20f6d8038df47d
Dylan <144082336+dylansantwani@users.noreply.github.com>
2025-09-15 13:07:20 -0500

    Update StackDemo.java

--- Chapter 15 Class Notes/src/StackDemo.java
+++ Chapter 15 Class Notes/src/StackDemo.java
@@ -4,10 +4,23 @@ import java.util.Stack;
  * This program simulates an undo stack. Note that operations
  * must be undone in the opposite order in which they are first
  * issued.
-*/
-public class StackDemo
-{
-    public static void main(String[] args)
-    {
+ */
+public class StackDemo {
+    public static void main(String[] args) {
+        Stack<String> commands = new Stack<>();
+
+        // push a bunch of commands onto the stack
+        commands.push("Insert: Hello");
+        commands.push("Insert: ','");
+        commands.push("Insert: ' '");
+        commands.push("Insert: 'World'");
+        commands.push("Insert: '?'");
+        commands.push("Insert: '?'");
+        commands.push("Insert: '!'");
+
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Undo" + commands.pop());
+        }
     }
 }
