88b0f0fdaca4e4bb20f14218d772b832dd437560
Dylan <144082336+dylansantwani@users.noreply.github.com>
2025-09-16 13:27:51 -0500

    do queue demos

--- Chapter 15 Class Notes/src/PriorityQueueDemo.java
+++ Chapter 15 Class Notes/src/PriorityQueueDemo.java
@@ -1,14 +1,27 @@
 import java.util.PriorityQueue;
 import java.util.Queue;
 
-
 /**
  * This program demonstrates a priority queue of to-do items. The
  * most important to-do items are removed first.
-*/
-public class PriorityQueueDemo
-{
-    public static void main(String[] args)
-    {
+ */
+public class PriorityQueueDemo {
+    public static void main(String[] args) {
+        // create a priority queue of strings
+        // needs to be composed of comparable objects
+        Queue<String> students = new PriorityQueue<>();
+        students.add("Neel");
+        students.add("Johnathan");
+        students.add("Cam");
+        students.add("Kaitlyn");
+        students.add("Dylan");
+        //the next highest priority object is moved to the front of the queue when the head of the queue is removed
+        while (!students.isEmpty()) {
+            System.out.println(students.remove()); 
+        }  
+        //create a to do list with priorities
+        Queue<WorkOrder> toDo = new PriorityQueue<>();
+        //lower priority numbers are more important
+        toDo.add(new WorkOrder(4, "Do the dishes"));
     }
 }
--- Chapter 15 Class Notes/src/QueueDemo.java
+++ Chapter 15 Class Notes/src/QueueDemo.java
@@ -4,10 +4,23 @@ import java.util.Queue;
 /**
  * This program simulates a print queue. Note that documents are printed
  * in the same order as they are submitted.
-*/
-public class QueueDemo
-{
-    public static void main(String[] args)
-    {
+ */
+public class QueueDemo {
+    public static void main(String[] args) {
+        // create print queue of strings using a linked list
+        Queue<String> jobs = new LinkedList<>();
+        jobs.add("Harry: Quarter 2 Expense Report");
+        jobs.add("Connor: Recipe for Banana Bread");
+        jobs.add("Katherine: Top Secret Document");
+        System.out.println("Printing " + jobs.remove());
+        jobs.add("Vardhan: Grocery List");
+        jobs.add("Katherine: Really Top Secret Document");
+        System.out.println("Printing " + jobs.remove());
+        System.out.println("Printing " + jobs.remove());
+        //print the rest of the jobs in the queue
+        while (!jobs.isEmpty()) {
+            System.out.println("Printing " + jobs.remove()); 
+        }   
+
     }
 }
