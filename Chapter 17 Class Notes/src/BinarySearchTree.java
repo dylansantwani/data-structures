9e54385d4a2a57cd31900ed3434602405ff06240
Dylan <144082336+dylansantwani@users.noreply.github.com>
2025-11-05 13:35:19 -0600

    Update BinarySearchTree.java

--- Chapter 17 Class Notes/src/BinarySearchTree.java
+++ Chapter 17 Class Notes/src/BinarySearchTree.java
@@ -1,84 +1,161 @@
+import java.util.*;
+
 /**
-    This class implements a binary search tree whose
-    nodes hold objects that implement the Comparable
-    interface.
-*/
-public class BinarySearchTree
-{   
+ * This class implements a binary search tree whose
+ * nodes hold objects that implement the Comparable
+ * interface.
+ */
+public class BinarySearchTree {
     private Node root;
 
     /**
-        Constructs an empty tree.
-    */
-    public BinarySearchTree()
-    {   
-        
-    }
-    
-    /**
-        Inserts a new node into the tree.
-        @param obj the object to insert
-    */
-    public void add(Comparable obj) 
-    {   
-        
+     * Constructs an empty tree.
+     */
+    public BinarySearchTree() {
+        root = null;
+        root.right = null;
+        root.left = null;
     }
 
     /**
-        Tries to find an object in the tree.
-        @param obj the object to find
-        @return true if the object is contained in the tree
-    */
-    public boolean find(Comparable obj)
-    {
+     * Inserts a new node into the tree.
+     * 
+     * @param obj the object to insert
+     */
+    public void add(Comparable obj) {
+        Node newnode = new Node();
+        newnode.data = obj;
+        newnode.left = null;
+        newnode.right = null;
+        if (root.left == null && root.right == null && root.data == null) {
+            root = newnode;
+        } else {
+            root.addNode(newnode);
+        }
+    }
+
+    /**
+     * Tries to find an object in the tree.
+     * 
+     * @param obj the object to find
+     * @return true if the object is contained in the tree
+     */
+    public boolean find(Comparable obj) {
+        if (root.data.equals(obj)) {
+            return true;
+        }
+        if (root.left != null) {
+            if (root.left.data.compareTo(obj) < 0) {
+                return false;
+            } else {
+                BinarySearchTree subtree = new BinarySearchTree();
+                subtree.root = root.left;
+                return subtree.find(obj);
+            }
+        }
+        if (root.right != null) {
+            if (root.right.data.compareTo(obj) > 0) {
+                return false;
+            } else {
+                BinarySearchTree subtree = new BinarySearchTree();
+                subtree.root = root.right;
+                return subtree.find(obj);
+            }
+        }
         return false;
     }
-    
-    /**
-        Tries to remove an object from the tree. Does nothing
-        if the object is not contained in the tree.
-        @param obj the object to remove
-    */
-    public void remove(Comparable obj)
-    {
-        
-    }
-    
-    /**
-        Prints the contents of the tree in sorted order.
-    */
-    public void print()
-    {   
-        
-    }   
 
     /**
-        Prints a node and all of its descendants in sorted order.
-        @param parent the root of the subtree to print
-    */
-    private static void print(Node parent)
-    {   
-        
+     * Tries to remove an object from the tree. Does nothing
+     * if the object is not contained in the tree.
+     * 
+     * @param obj the object to remove
+     */
+    public void remove(Comparable obj) { //to complete
+        if (root.data.equals(obj)) {
+            if (root.left == null && root.right == null){
+                root = null;
+            }
+            else if (root.left != null && root.right == null){
+                root = root.left;
+            }
+            else if (root.right != null && root.left == null){
+                root = root.right;
+            }
+            else{
+                //go to the nodes right child then all the way to the left then use that node to replace the root
+            }
+            
+            
+        }
+        if (root.left != null){
+            if (root.left.data.compareTo(obj) < 0) {
+                return;
+            } else {
+                BinarySearchTree subtree = new BinarySearchTree();
+                subtree.root = root.left;
+                subtree.remove(obj);
+            }
+        }if (root.right != null){
+            if (root.right.data.compareTo(obj) > 0) {
+                return;
+            } else {
+                BinarySearchTree subtree = new BinarySearchTree();
+                subtree.root = root.right;
+                subtree.remove(obj);
+            }
+        }
     }
 
     /**
-        A node of a tree stores a data item and references
-        to the left and right child nodes.
-    */
-    static class Node
-    {   
-        
+     * Prints the contents of the tree in sorted order.
+     */
+    public void print() {
+
+    }
+
+    /**
+     * Prints a node and all of its descendants in sorted order.
+     * 
+     * @param parent the root of the subtree to print
+     */
+    private static void print(Node parent) {
+
+    }
+
+    /**
+     * A node of a tree stores a data item and references
+     * to the left and right child nodes.
+     */
+    static class Node {
+
+        public Comparable data;
+        public Node left;
+        public Node right;
 
         /**
-            Inserts a new node as a descendant of this node.
-            @param newNode the node to insert
-        */
-        public void addNode(Node newNode)
-        {   
-            
+         * Inserts a new node as a descendant of this node.
+         * 
+         * @param newNode the node to insert
+         */
+        public void addNode(Node newNode) {
+            if (newNode.data.compareTo(this.data) < 0) {
+                if (this.left == null) {
+                    this.left = newNode;
+                } else {
+                    this.left.addNode(newNode);
+                }
+            } 
+            else if (newNode.data.compareTo(this.data) == 0){
+                return;
+            }
+            else {
+                if (this.right == null) {
+                    this.right = newNode;
+                } else {
+                    this.right.addNode(newNode);
+                }
+            }
         }
     }
 }
-
-
-
